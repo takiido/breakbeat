bb0e670d98e328c74260e7d9dfb91946c56d422e
takiido <takiid0@proton.me>
2026-05-08 15:11:50 -0500

    created user model

---
+++ api/src/auth/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlachemy import String, DateTime, func
+from sqlachemy.orm import Mapped, mapped_column
+
+
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
