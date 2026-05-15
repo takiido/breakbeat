0001328b366ed924b2bb5cbdcdf20edc02cee6b4
takiido <takiid0@proton.me>
2026-05-14 20:08:03 -0500

    feat: add logging configuration for fetch requests in Next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: false,
+    }
+  }
 };
 
 export default nextConfig;
